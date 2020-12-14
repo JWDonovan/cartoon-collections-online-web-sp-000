f948dccad848b55234d4c07ed51f444bbe5bb159
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 21:33:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,12 @@ def roll_call_dwarves(names)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  veggies.collect do |veggie|
+    veggie.capitalize
+    veggie << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
